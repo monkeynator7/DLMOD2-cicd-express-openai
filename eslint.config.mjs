c9de3a0a501c311ff47dc2eaf40409aa7c1a0513
Monica Carvajal <mcarvajalrm@gmail.com>
2025-09-02 22:04:30 -0400

    erase missing eslint react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,5 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } }
 ]);
