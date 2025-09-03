4332d50db1d9b718d135e440be1f4ae6c59c0ebb
Monica Carvajal <mcarvajalrm@gmail.com>
2025-09-02 22:03:09 -0400

    update eslint react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
